f36b6335c5a738d98d426b7e80eec4c4fcaee6d4
Kinyua <alfkinyua@gmail.com>
2022-10-11 17:17:49 +0300

    Refactor obj to readable semantics

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,16 +3,16 @@ require 'date'
 
 describe Game do
   before :each do
-    @obj = Game.new(true, '2022-10-10', '1998-10-11')
+    @game = Game.new(true, '2022-10-10', '1998-10-11')
   end
   context 'create an instance of game with game object' do
     it 'takes three parameters and return a Game object' do
-      expect(@obj).to be_instance_of Game
+      expect(@game).to be_instance_of Game
     end
   end
   context 'When archiving a game' do
     it 'archives the game' do
-      expect(@obj.can_be_archived?).to eq(true)
+      expect(@game.can_be_archived?).to eq(true)
     end
   end
 end
