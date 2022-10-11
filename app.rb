d31f63dfc9dac4832f1f9d5fe8a33d34e1f7871e
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-11 16:10:57 +0200

    Create add and store label methods

--- app.rb
+++ app.rb
@@ -1,11 +1,28 @@
 require './book'
+require './label'
 
 class App
-  def list_all_books
-    books = File.size('./books.json').zero? ? [] : JSON.parse(File.read('./books.json'))
-    books.each do |b|
-      puts "Publisher: #{b['publisher']}, Cover State: #{b['cover_state']}, Publish Date: #{b['publish_date']}"
-    end
+  def add_label(item)
+    print 'Label title: '
+    title = gets.chomp
+    print 'Label color: '
+    color = gets.chomp
+
+    label = Label.new(title, color)
+    label.add_item(item)
+    store_label(label)
+  end
+
+  def store_label(label)
+    hash = {
+      id: label.id,
+      title: label.title,
+      color: label.color,
+    }
+
+    file = File.size('./labels.json').zero? ? [] : JSON.parse(File.read('./labels.json'))
+    file << hash
+    File.write('labels.json', file.to_json)
   end
 
   def add_a_book
@@ -17,17 +34,32 @@ class App
     publish_date = gets.chomp
 
     book = Book.new(publisher, cover_state, publish_date)
+    add_label(book)
+    store_book(book)
+  end
+
+  def store_book(book)
     hash = {
       id: book.id,
       publisher: book.publisher,
       archived: book.archived,
       cover_state: book.cover_state,
       publish_date: book.publish_date,
-      author_id: book.author ? book.author.id : nil
+      label_id: book.label.id
     }
 
     file = File.size('./books.json').zero? ? [] : JSON.parse(File.read('./books.json'))
     file << hash
     File.write('books.json', file.to_json)
   end
+
+
+  def list_all_books
+    books = File.size('./books.json').zero? ? [] : JSON.parse(File.read('./books.json'))
+    books.each do |b|
+      puts "Publisher: #{b['publisher']}, Cover State: #{b['cover_state']}, Publish Date: #{b['publish_date']}"
+    end
+  end
+
+
 end
--- label.rb
+++ label.rb
@@ -6,6 +6,7 @@ class Label
     @id = Random.rand(1..1000)
     @title = title
     @color = color
+    @items = []
   end
 
   def add_item(item)
